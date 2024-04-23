a436af708c82eb99b8beb8e5010f2df0cee95d5c
bilal1465 <sarwar.bilal1465@gmail.com>
2024-04-22 21:52:55 -0600

    altered colours of buttons and icons. featured section complete.

--- client/src/components/FeaturedCards.js
+++ client/src/components/FeaturedCards.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function FeaturedCards(props) {
   return (
-    <div className='w-1/6 h-1/2'>
+    <div className='flex flex-col w-1/6 h-1/2'>
         <div className='bg-green-400 h-64 w-full bg-cover bg-center overflow-hidden' style={{backgroundImage: `url(${props.image})`}}>
         </div>
-        <h1 className='text-white font-bold' >{props.title}</h1>
-        <button className='bg-green-600 rounded-full p-2 w-1/2 mt-4 text-white font-extrabold' >See Menu</button>
+        <h1 className='text-white font-bold pt-2'>{props.title}</h1>
+        <button className='bg-gradient-to-r from-[#1d8f7f] to-[#1b9985] rounded-full p-2 w-1/2 mt-4 text-white font-extrabold m-auto ' >See Menu</button>
     </div>
   )
 }
--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -6,26 +6,26 @@ function Hero() {
     <div className='h-svh' >
         <div className="flex items-center justify-center flex-row bg-bottom bg-cover h-4/6 space-x-28" style={{backgroundImage: "url('restaurant_hero.jpg')"}}>
           <div className='flex flex-col items-center justify-center'>
-            <div className='flex items-center justify-center bg-green-500 rounded-full p-4 h-min w-min'>
+            <div className='flex items-center justify-center bg-[#1b9985] rounded-full p-4 h-min w-min'>
               <GiChiliPepper className='size-10 text-white' />
             </div>
             <h1 className='text-white text-2xl' ><a>Desi</a></h1>           
           </div>
           <div className='flex flex-col items-center justify-center'>
-            <div className='flex items-center justify-center bg-green-500 rounded-full p-4 h-min w-min'>
+            <div className='flex items-center justify-center bg-[#1b9985] rounded-full p-4 h-min w-min'>
               <GiOlive className='size-10 text-white' />
             </div>
             <h1 className='text-white text-2xl' ><a>Arab</a></h1>            
           </div>
           <div className='flex flex-col items-center justify-center'>
-            <div className='flex items-center justify-center bg-green-500 rounded-full p-4 h-min w-min'>
+            <div className='flex items-center justify-center bg-[#1b9985] rounded-full p-4 h-min w-min'>
               <GiHamburger className='size-10 text-white' />
             </div>        
             <h1 className='text-white text-2xl' ><a>Western</a></h1>    
           </div>          
         </div>
         <div className='pt-12'>
-          <h1 className='m-auto border-[1px] border-[#1d8f7f] rounded p-4 text-center w-1/2 text-4xl text-white'>Every halal restaurant in one place.</h1>
+          <h1 className='m-auto border-[1px] border-[#1b9985] rounded p-4 text-center w-1/2 text-4xl text-white'>Every halal restaurant in one place.</h1>
         </div>
         <h1 className='text-white mt-12 pl-8 text-4xl' >Featured: Calgary's Choice</h1>
     </div>    
