09aab9ae96b20701eaefced1bbe1f655f235af88
bilal1465 <sarwar.bilal1465@gmail.com>
2024-04-27 15:42:32 -0600

    added filter by country feature on desi page. Trying to make filter feature a reusable component for other pages

--- client/src/pages/Desi.js
+++ client/src/pages/Desi.js
@@ -4,7 +4,11 @@ import RestaurantCards from '../components/RestaurantCards'
 
 function Desi() {
 
-  const [desiRestaurants, setDesiRestaurants] = useState(null);
+  const [desiRestaurants, setDesiRestaurants] = useState([]);
+
+  let countries = ["Any"]
+
+  const [selectedCountry, setCountry] = useState("")
 
   useEffect(() => {
     const getData = async () => {
@@ -15,21 +19,42 @@ function Desi() {
     getData()
   }, [])
 
+  desiRestaurants.map((restaurant) => {
+    if (!countries.includes(restaurant.country)) {
+      countries.push(restaurant.country)
+    }
+  })    
+
   return (
     <div className='h-svh' >
       <div className="flex items-center justify-center bg-cover h-4/6" style={{backgroundImage: "url('media/desi.jpg')"}}>
         <HeroNav />
       </div>
       <div className='p-4' >
-        <div className='flex flex-row p-4 space-x-6 rounded'>
-          {desiRestaurants && desiRestaurants.map(card => (
-                <RestaurantCards
-                    className='w-2/3 h-2/3'
-                    key={card.id}
-                    title={card.title}
-                    image={card.image}
-                />
+        <label for="filter-country" className="text-white ml-3" >Country: </label>
+        <select onChange={(event) => setCountry(event.target.value)} id="filter-country" className="w-1/6 rounded border border-black bg-white pl-1">
+            {countries && countries.map((country,index) => (
+                <option value={country} key={index}>{country}</option>
             ))}
+        </select>
+        <div className='flex flex-row p-4 space-x-6 rounded'>
+          {desiRestaurants && desiRestaurants.map((restaurant,index) => {
+            if (restaurant.country == selectedCountry) {              
+              return <RestaurantCards
+              className='w-2/3 h-2/3'
+              key={index}
+              title={restaurant.title}
+              image={restaurant.image}
+              />
+            } else if (selectedCountry == "Any") {
+              return <RestaurantCards
+              className='w-2/3 h-2/3'
+              key={index}
+              title={restaurant.title}
+              image={restaurant.image}
+              />
+            }
+})}
         </div>
       </div>
     </div>
